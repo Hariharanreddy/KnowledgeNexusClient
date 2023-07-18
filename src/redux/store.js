fc4afa909ab75b0fde5663e2a03ea0010e03308c
Hariharanreddy <animatrix536@gmail.com>
2023-07-18 20:15:53 +0530

    changedto cyclic server

--- src/redux/store.js
+++ src/redux/store.js
@@ -24,5 +24,5 @@ export default store;
 // export const server = 'https://knowledge-nexus-server-new.vercel.app/api/v1';
 // export const server = 'http://localhost:4000/api/v1';
 // export const server = 'https://knowledgenexusrender.onrender.com/api/v1';
-// export const server= 'https://knowledge-nexus-server.cyclic.app/api/v1';
-export const server= 'https://knowledgenexusserver-production.up.railway.app/api/v1';
+export const server= 'https://knowledge-nexus-server.cyclic.app/api/v1';
+// export const server= 'https://knowledgenexusserver-production.up.railway.app/api/v1';
